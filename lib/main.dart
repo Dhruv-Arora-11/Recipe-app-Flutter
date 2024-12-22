e8bbcfc61d94cdb1749d00f1ab3a89b651edbfa6
Dhruv Arora <168272203+Dhruv-Arora-11@users.noreply.github.com>
2024-12-22 11:48:16 +0530

    started the project
    StartProject

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/homepage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-c
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Homepage(),
+    );
+  }
+}
